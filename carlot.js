19b155415144aa89c517f7dc16a0543c77d22621
Debbie Bourne <dblashrink@hotmail.com>
2016-08-05 23:10:59 -0500

    fixed broken IIFE. worked on key bindings

--- carlot.js
+++ carlot.js
@@ -3,11 +3,12 @@ console.log("carlot.js is here")
 var CarLot = (function () {
   var inventory = [];
   console.log(inventory);
-
   return {
+    //this makes the inventory available if I need to get it for a different function outside the iife
     getInventory: function () {
       return inventory
     },
+    //this loads the cars from the json file and send it to populate
     loadInventory: function (callback) {
       console.log("loadInventory starts");
       var inventoryLoader = new XMLHttpRequest();
@@ -23,6 +24,54 @@ var CarLot = (function () {
     inventoryLoader.send()
     }
   };
+}());
 
-})();
+var CarLot = (function (cl) {
+  cl.activateEvents = function (callback) {
+      console.log("activate events is on deck"); //we're getting here
+      var carDivClick = document.querySelectorAll(".col-sm-3"); //cars
+      console.log(carDivClick) //it finds the four car elements
+      // var buttonClicked = document.querySelector("#textInputButton"); //button
+      // console.log(buttonClicked)  //it finds the button
+      // var carTarget
+      // for(i = 0; i > carDivClick.length; i++) {
+      //   carDivClick[i].addEventListener("click", function(evt) {
+      //since there are four items that have this class, it lists all of them as an array, so we need to loop through the cars adding a listener to each. for some reason the above for loop did not work (it would not log a click event but the below one did.
+      for (var i = carDivClick.length - 1; i >= 0; i--) {
+        carDivClick[i].addEventListener("click", function(evt) {
+        console.log(evt);
+        console.log(event.currentTarget);
+        // carTarget = event.currentTarget
+        // console.log(carTarget)
+        changeStuffOnPage(event.currentTarget)
+        })
+      }
+  }
+  return (cl)
+}(CarLot));
 
+// carlot || {}  //could use this?
+
+function changeStuffOnPage (cl) {
+  console.log("changestuffonpage is here")
+  document.getElementById("textInputField").removeAttribute("disabled");
+  document.getElementById("textInputButton").removeAttribute("disabled");
+  event.currentTarget.removeAttribute("style");
+  event.currentTarget.classList.add("bigBorder");
+  // var addDescription = event.currentTarget.childNode[6];
+  // console.log(addDescription);
+  // getElementById("description");
+
+  document.getElementById("textInputField").addEventListener("keydown", function(evt) {
+    console.log(evt.target.value);
+    // document.getElementById("textInputField").value = "";
+    // event.currentTarget.childNode[6].innerHTML += evt.key;
+    // console.log(addDescription);
+    // addDescription.innerHTML += evt.key;
+    // var eventKey = evt.key
+    // console.log(eventKey);
+    //output.innerHTML = event.target.value;
+    //binding
+
+  })
+};
--- quiz_xhr.js
+++ quiz_xhr.js
@@ -5,12 +5,12 @@ function populatePage (inventory) {
   var inventoryPost = document.getElementById("carsGoHere");
   console.log("inventory", inventory);
   inventory.forEach (function(car) {
-    inventoryPost.innerHTML += `<div class="col-sm-3" style="border:${car.color} solid 1px"><p class>Make: ${car.make}</p><p>Model: ${car.model}</p><p>Year: ${car.year}</p><p>Price: ${car.price}</p><p>Color: ${car.color}</p><p>Purchased: ${car.purchased}</p><p>Description: ${car.description}</p></div>`
-
-    console.log(car.color);
+    inventoryPost.innerHTML += `<div class="col-sm-3 ${car.id}" style="border:${car.color} solid 1px"><p class>Make: ${car.make}</p><p>Model: ${car.model}</p><p>Year: ${car.year}</p><p>Price: ${car.price}</p><p>Color: ${car.color}</p><p>Purchased: ${car.purchased}</p><p id="description">Description: ${car.description}</p></div>`
+    // console.log(car.color); used this to figure out how to push object color into HTML for border
   })
    // Now that the DOM is loaded, establish all the event listeners needed
-  // CarLot.activateEvents();
+  CarLot.activateEvents(changeStuffOnPage);
 }
 
 CarLot.loadInventory(populatePage);
+
